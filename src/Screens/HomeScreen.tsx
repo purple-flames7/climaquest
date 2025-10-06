acb2a7c70ddfa0c0dd267c8a2f0ace125b0e18da
Nicole Katia <nicolekatia04@gmail.com>
2025-10-06 13:04:01 +0300

    feat: implement SplashScreen with animated logo

---
+++ src/Screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+// src/components/HomeScreen.tsx
+export default function HomeScreen() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-green-600">
+      <h1 className="text-4xl md:text-6xl text-white font-bold">
+        Home Screen Placeholder
+      </h1>
+    </div>
+  );
+}
